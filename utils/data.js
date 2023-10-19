7a51e47dd90fb42dd8ca301bd2291b3fb3c3a7ee
maliacho <maliacho21@gmail.com>
2023-10-18 23:16:32 -0700

    began to refactor data.js and seed.js

--- utils/data.js
+++ utils/data.js
@@ -74,40 +74,38 @@ const names = [
     'Parker',
 ];
 
-const appDescriptions = [
-    'Decision Tracker',
-    'Find My Phone',
-    'Learn Piano',
-    'Starbase Defender',
-    'Tower Defense',
-    'Monopoly Money Manager',
-    'Movie trailers',
-    'Hello world',
-    'Stupid Social Media App',
-    'Notes',
-    'Messages',
-    'Email',
-    'Compass',
-    'Firefox',
-    'Running app',
-    'Cooking app',
-    'Poker',
-    'Deliveries',
+const thoughtDescriptions = [
+    'Chocolate Chip Cookies Are The Best Cookie',
+    'BLACKPINK is my favorite girl-group',
+    'I want to learn how to play piano',
+    'Vanilla Lattes are so yummy',
+    'I love cooking',
+    'Monopoly is such a boring game',
+    'Movie trailers have gotten so bad',
+    'Hello world, my name is Taylor Swift',
+    'Social media is so addicting',
+    'How do you like to take notes?',
+    'I have so many unread messages',
+    'Do you check your email regularly?',
+    'How does a compass work?',
+    'Chrome or Safari- which is better?',
+    'I want to download a running app. I was thinking the Nike one',
+    'I love finding new recipes!',
+    'Do you play poker? I want to learn',
+    'I make deliveries for DoorDash',
 ];
 
-// Get a random item given an array
 const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-// Gets a random full name
 const getRandomName = () =>
     `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
 
 // Function to generate random assignments that we can add to student object.
-const getRandomAssignments = (int) => {
+const getRandomThoughts = (int) => {
     const results = [];
     for (let i = 0; i < int; i++) {
         results.push({
-            assignmentName: getRandomArrItem(appDescriptions),
+            thoughtName: getRandomArrItem(thoughtDescriptions),
             score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
         });
     }
@@ -115,4 +113,4 @@ const getRandomAssignments = (int) => {
 };
 
 // Export the functions for use in seed.js
-module.exports = { getRandomName, getRandomAssignments };
+module.exports = { getRandomName, getRandomThoughts };
